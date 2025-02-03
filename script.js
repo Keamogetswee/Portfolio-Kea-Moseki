628a3f489a565eba572ac66f0cda358edca1fba9
Kea <neoentlemoseki@gmail.com>
2025-02-03 18:09:39 +0200

    Interactivity to navigation menu

---
+++ script.js
@@ -0,0 +1,26 @@
+// To show the menu
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+if(navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+// removing the menu on mobile
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+    // when clicking on each nav linkAction, we remove the show menu
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
